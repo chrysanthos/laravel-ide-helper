a7cfca0557b54e3e70c8e7ac67b4fc9e9cf23057
Chrysanthos <48060191+chrysanthos@users.noreply.github.com>
2021-07-17 23:08:18 +0300

    wip

--- src/Macro.php
+++ src/Macro.php
@@ -74,11 +74,11 @@ class Macro extends Method
         }
 
         /** @var \ReflectionMethod $enclosingMethod */
-        $enclosingMethod = Collection::make($enclosingClass->getMethods())
+        $enclosingMethod = $enclosingClass ? Collection::make($enclosingClass->getMethods())
             ->first(function (\ReflectionMethod $method) {
                 return $method->getStartLine() <= $this->method->getStartLine()
                     && $method->getEndLine() >= $this->method->getEndLine();
-            });
+            }) : null;
 
         if ($enclosingMethod) {
             $this->phpdoc->appendTag(Tag::createInstance(
